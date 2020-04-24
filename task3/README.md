e6ee614becc31ba50e0d9a4f426b93fdc0ba5e05
mjumnir <mjumnir@gmail.com>
2020-04-24 04:12:53 +0000

    Fixing issues

--- task3/README.md
+++ task3/README.md
@@ -1,4 +1,6 @@
 # WAD TASK 3
+*Create a login/register apllication*
+*No Database Using Flask-Login*
 
 # UI Credit
 https://codepen.io/ehermanson/pen/KwKWEv
--- task3/logic/user.py
+++ task3/logic/user.py
@@ -1,34 +1,42 @@
-from werkzeug.security import generate_password_hash, check_password_hash
+from werkzeug.security import check_password_hash
 from flask_login import UserMixin
 import os
 
+user_database = {
+    'User':{
+        'john@1.com': {
+            'name': 'John Doe',
+            'mail': 'john@1.com',
+            'uname': 'John',
+            'pswd': 'pbkdf2:sha256:150000$FB96URjN$86ea2a5f5435b20aa981184bb770bc5b5fb17f865ae9e2b0048bf3bed0a6f80a'
+            },
+        'janedoe@wad.com': {
+            'name': 'Jane Doe',
+            'mail': 'janedoe@wad.com',
+            'uname': 'Jane',
+            'pswd': 'pbkdf2:sha256:150000$HHKgHv5c$fbe8640afcd778891720b6ac261ae0272015a9308f56a21ed32ff296ce524563'
+            },
+        'magnie@m.com': {
+            'name': 'Magnie Dess',
+            'mail': 'magnie@yandex.com',
+            'uname': 'Magnie',
+            'pswd': 'pbkdf2:sha256:150000$32otDXGB$03734879ba6f0700af2defa70c4b182fefa1eb4281ad65a9021d408a61725a3f'
+            },
+        'test@t.com': {
+            'name': 'Test',
+            'mail': 'test@t.com',
+            'uname': 'Test',
+            'pswd': 'pbkdf2:sha256:150000$I7M0C4bf$406d9f83bd9777baa915b2e37299a4e53255f471e8cf7a0cfb58ad8d2fb45ad9'}
+    }
+}
+
 class User(UserMixin):
+    pass
 
-    user_database = {"John@1.com": ("John@1.com", "John"),
-               "JaneDoe": ("JaneDoe", "Jane"),
-               "Mjumnir": ('magnie@yandex.com', 'magnie'),
-               "Test": ('test@test.com', '1')}
 
-    def __init__(self, username, password):
-        self.id = username
-        self.password = password
-
-    def is_active(self):
-        """True, as all users are active."""
-        return True
-
-    def get_id(self):
-        """Return the email address to satisfy Flask-Login's requirements."""
-        return self.id
-
-    def is_authenticated(self):
-        """Return True if the user is authenticated."""
-        return self.authenticated
-
-    def is_anonymous(self):
-        """False, as anonymous users aren't supported."""
-        return False
-
-    @classmethod
-    def get(cls,id):
-        return cls.user_database.get(id)
+def get_user(self,mail,pswd):
+    if mail in user_database['User']:
+        root = user_database['User'][mail]
+        if check_password_hash(root['pswd'], pswd):
+            return self(root['mail'], mail)
+    return None
